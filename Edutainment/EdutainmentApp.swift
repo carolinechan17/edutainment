464b5f2852d076e9a11c7d7a35cacc28d0e8b0bf
Caroline Chan <carolinechan304@gmail.com>
2022-10-27 19:43:19 +0700

    Initial commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Caroline Chan on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
